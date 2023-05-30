d32abadbe235ef432f4a2cbbf38da6f5e11d4103
Kathalu-roshni <134576272+Kathalu-roshni@users.noreply.github.com>
2023-05-30 09:11:46 +0530

    Add files via upload

--- WelcomePage.java
+++ WelcomePage.java
@@ -6,15 +6,16 @@ public class WelcomePage {
     JFrame frame = new JFrame();
     JLabel welcomeLabel = new JLabel("Hello!");
 
-    WelcomePage(String userID){
+    WelcomePage(String userID) {
 
-        welcomeLabel.setBounds(0,0,200,35);
-        welcomeLabel.setFont(new Font(null,Font.PLAIN,25));
-        welcomeLabel.setText("Hello "+userID);
+        welcomeLabel.setBounds(0, 0, 200, 35);
+        welcomeLabel.setFont(new Font(null, Font.PLAIN, 25));
+        welcomeLabel.setText("Hello " + userID);
 
         frame.add(welcomeLabel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 420);
         frame.setLayout(null);
-        frame.setVisible(true);}
+        frame.setVisible(true);
+    }
 }
